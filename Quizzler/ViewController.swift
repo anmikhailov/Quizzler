2f8e6f19fa2a842aa8ab7340619701056864a91c
Andrey <mands315@gmail.com>
2023-04-10 17:58:50 +0300

    Add icon, layout and set connections between Main.storyboard and ViewController.swift

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: - Outlets
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    let quiz = [
+        "Four + Two is equal to Six",
+        "Five - Three is equal to One",
+        "Three + Eight is less than Ten"
+    ]
+    
+    var questionNumber = 0
+    
+    //MARK: - LifeCycles
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        updateUI()
     }
-
-
+    
+    //MARK: - Actions
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        questionNumber += 1
+        updateUI()
+    }
+    
+    func updateUI() {
+        questionLabel.text = quiz[questionNumber]
+    }
 }
 
